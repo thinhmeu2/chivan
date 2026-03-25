e160d24a68fbc7e8248eb97af40a7945af431f82
thinh <thinhmeu.tntn@gmail.com>
2026-03-25 16:13:16 +0700

    de3cang

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Keys\De3Cang;
 use Illuminate\Support\Facades\Route;
 
 Route::get('soicau', function (){
-    (new DanDe10_3())->init();
+//    (new DanDe10_3())->init();
 //    (new DanDe20_3())->init();
 //    (new DanDe30_3())->init();
-//    (new De3Cang())->init();
+    (new De3Cang())->init();
 });
 Route::get('{slug}', [\App\Http\Controllers\HomeController::class, 'index'])->where('any', '.*');
